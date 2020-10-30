2e858dbc91a9a0cdfd3ac9581f528d1cde2ed1de
cho <whdydgur10@gmail.com>
2020-10-30 16:47:43 +0900

    JAVA : Import

---
+++ java/src/day10/Import예제.java
@@ -0,0 +1,14 @@
+package day10;
+
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class Import예제 {
+
+	public static void main(String[] args) {
+		System.out.println(random());
+		out.println(random());
+		
+	}
+
+}
