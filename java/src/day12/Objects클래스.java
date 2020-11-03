d4bc040631fced171c70dd7dcb8d19557fb45f43
cho <whdydgur10@gmail.com>
2020-11-03 16:40:01 +0900

    JAVA : 유용한클래스 : Objects클래스

---
+++ java/src/day12/Objects클래스.java
@@ -0,0 +1,18 @@
+package day12;
+
+import java.util.Objects;
+
+public class Objects클래스 {
+
+	public static void main(String[] args) {
+		Integer num1 = null;
+		System.out.println("num1은 null이 아닌가? "+Objects.nonNull(num1));
+		System.out.println("num1은 null인가? "+Objects.isNull(num1));
+		Integer num2 = 30;
+		//Objects.equals 는 값이 null이여도 상관 없음. 
+		System.out.println("num1과 num2가 같은가? "+Objects.equals(num1, num2));
+		//Object.equals는 널포인트 예외 발생.
+		System.out.println("num1과 num2가 같은가? "+ num1.equals(num2));
+	}
+
+}
